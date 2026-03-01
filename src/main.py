8fd3b566e295435df9effad125fa5d7fadbd4d58
royal_roo <cj311288@gmail.com>
2026-02-28 17:08:35 -0800

    proper image placement

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from generate_page import generate_page, generate_pages_recursive
 import sys
 def main():
     basepath = sys.argv[1] or "/"
+    copy_source(src="static", dest="docs", clean=True)
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
