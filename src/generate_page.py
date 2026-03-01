dd5bce4efcfeae452260c68098cd7b442b9632a0
royal_roo <cj311288@gmail.com>
2026-02-28 16:37:25 -0800

    recusrive page generation function

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 from extract_title import extract_title
 import os
@@ -18,6 +19,26 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(temp_file)
 
-
-
-
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    current_directory = os.listdir(dir_path_content)
+    for item in current_directory:
+        current_path = os.path.join(dir_path_content, item)
+        current_dest_path = os.path.join(dest_dir_path, item)
+        if os.path.isfile(current_path):
+            path = Path(current_dest_path)
+            fixed_path = path.with_suffix(".html")
+            with open(current_path) as f:
+                md = f.read()
+            with open(template_path) as f:
+                temp_file = f.read()
+            md_node = markdown_to_html_node(md)
+            html_string = md_node.to_html()
+            title = extract_title(md)
+            temp_file = temp_file.replace("{{ Title }}", title)
+            temp_file = temp_file.replace("{{ Content }}", html_string)
+            dest_dir = os.path.dirname(fixed_path)
+            os.makedirs(dest_dir, exist_ok=True)
+            with open(fixed_path, "w") as f:
+                f.write(temp_file)
+        else:
+            generate_pages_recursive(current_path, template_path, current_dest_path)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from textnode import *
 from copystatic import copy_source
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 def main():
     copy_source()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 main()
